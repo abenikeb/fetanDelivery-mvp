6be5e06b79e37230abb527891b92701c26a93a21
Abenezer Kebede <92208254+ebenizerk@users.noreply.github.com>
2022-03-01 17:06:06 +0300

    add contents

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
+require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql");
+
 const app = express();
-const PORT = 5000;
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fetan-db",
-});
+async function startServer() {
+  port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server is listing on port ${port}`);
+  });
+}
 
-db.connect((err) => {
-  if (err) throw new Error("Error Found!");
-  console.log("DB Connected Successfully ");
-});
-
-// app.get("/getEmployeeById_", (req, res) => {
-//   var sql =
-//     "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), address VARCHAR(250))";
-
-//   db.query(sql, (err, results) => {
-//     if (err) console.log(err);
-//     res.send(results);
-//   });
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server is listing on port ${PORT}`);
-});
+startServer();
